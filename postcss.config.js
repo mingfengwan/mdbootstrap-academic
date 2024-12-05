ddabde2bdc8937feb2ff8d89941f148b8f7abf2c
mingfengwan <ming@explorerhop.com>
2024-12-05 02:11:04 -0500

    Optimize import to reduce file size. Minor html changes.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 const purgecss = require('@fullhuman/postcss-purgecss')
 
 const purgecssConfig = purgecss({
-  content: ['./**/*.html', './node_modules/mdb-ui-kit/js/mdb.umd.min.js'],
+  content: ['./dist/*.html', './node_modules/mdb-ui-kit/js/mdb.umd.min.js'],
   css: ['./node-modules/mdb-ui-kit/**/*.css'],
 })
 
