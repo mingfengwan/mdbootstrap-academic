6ed9f51583e0fddafd8cdab6b2b93f0eaed5411d
mingfengwan <ming@explorerhop.com>
2024-12-04 22:19:30 -0500

    Add controls to carousel

--- src/js/photos.js
+++ src/js/photos.js
@@ -1,5 +1,10 @@
 import '../scss/styles.scss';
 
+import { faChevronLeft, faChevronRight } from '@fortawesome/pro-solid-svg-icons';
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+library.add(faChevronLeft, faChevronRight);
+dom.watch();
+
 import { Carousel, initMDB } from "mdb-ui-kit";
 
 initMDB({ Carousel });
