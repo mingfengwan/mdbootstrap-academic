66852aac4d7b6dc6a2b7c9a8e7e02372cc8fef53
mingfengwan <ming@explorerhop.com>
2024-12-07 02:47:33 -0500

    Updates to photos.html layout:
    1. Replace number text with Duotone numbers
    2. Center the carousel

--- src/js/photos.js
+++ src/js/photos.js
@@ -1,8 +1,9 @@
 import '../scss/photos.scss';
 
 import { faChevronLeft, faChevronRight } from '@fortawesome/pro-solid-svg-icons';
+import {faCircle5, faCircle4, faCircle3, faCircle2, faCircle1} from '@fortawesome/pro-duotone-svg-icons';
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-library.add(faChevronLeft, faChevronRight);
+library.add(faChevronLeft, faChevronRight, faCircle5, faCircle4, faCircle3, faCircle2, faCircle1);
 dom.watch();
 
 import { Carousel, initMDB } from "mdb-ui-kit";
