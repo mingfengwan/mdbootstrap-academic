ddd114abfba87d73634144bdc3b3ff42ca42ef0b
mingfengwan <ming@explorerhop.com>
2024-12-07 05:26:10 -0500

    Add floating hearts to the background of photos.

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 const purgecssConfig = purgecss({
   content: ['./dist/*.html', './node_modules/mdb-ui-kit/js/mdb.umd.min.js'],
   css: ['./node-modules/mdb-ui-kit/**/*.css'],
+  safelist: ['heart'], // Keep 'heart' class intact
 })
 
 module.exports = ({ env }) => ({
