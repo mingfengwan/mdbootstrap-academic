a53940c68a3db8c03806be1b062f1c6a058b9c05
mingfengwan <ming@explorerhop.com>
2024-12-07 23:17:51 -0500

    Fix build issue and update purgecss to 7.0.2

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,21 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
+const purgecss = require('@fullhuman/postcss-purgecss').default;
 
 const purgecssConfig = purgecss({
-  content: ['./dist/*.html', './node_modules/mdb-ui-kit/js/mdb.umd.min.js'],
-  css: ['./node-modules/mdb-ui-kit/**/*.css'],
-  safelist: ['heart'], // Keep 'heart' class intact
+    content: ['./dist/*.html', './node_modules/mdb-ui-kit/js/mdb.umd.min.js'],
+    css: ['./node_modules/mdb-ui-kit/**/*.css'],
+    safelist: {
+        standard: ['heart'], // Keep 'heart' class intact
+        deep: [
+            /^ripple-surface-*/, // Allow all classes starting with ripple-surface-
+            /^ripple-wave$/,     // Allow ripple-wave
+        ], // Allow deeply nested rules
+    },
 })
 
-module.exports = ({ env }) => ({
-  plugins: [
-    require('autoprefixer'),
-    require('postcss-nested'),
-    env === 'production' ? purgecssConfig : false,
-  ]
+module.exports = ({env}) => ({
+    plugins: [
+        require('autoprefixer'),
+        require('postcss-nested'),
+        env === 'production' ? purgecssConfig : false,
+    ]
 })
