d37ff86e8a4364d5f30295e44d32179c605d1871
mingfengwan <ming@explorerhop.com>
2024-12-07 19:02:42 -0500

    Remove photo text and move the link to a button instead. Change navbar color and navbrand icon color

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,8 @@ initMDB({ Collapse, Tooltip, Ripple });
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faPersonDressSimple, faEnvelope, faSparkles, faCommentSmile, faGlasses, faChartMixedUpCircleDollar, faFilePowerpoint, faFileLines, faFileCode, faHashtag, faBookOpen, faPresentationScreen, faVideo, faBookOpenReader, faChalkboardUser, faLocationDot, faSnowflake } from '@fortawesome/pro-duotone-svg-icons';
 import { faBars, faAngleDown } from '@fortawesome/pro-solid-svg-icons';
+import { faImage } from '@fortawesome/duotone-light-svg-icons/faImage';
 import { faGithubAlt, faLinkedinIn, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faPersonDressSimple, faBars, faEnvelope, faAngleDown, faSparkles, faCommentSmile, faGlasses, faChartMixedUpCircleDollar, faFilePowerpoint, faFileLines, faFileCode, faHashtag, faBookOpen, faPresentationScreen, faVideo, faBookOpenReader, faChalkboardUser, faLocationDot, faSnowflake, faGithubAlt, faLinkedinIn, faYoutube);
+library.add(faPersonDressSimple, faImage, faBars, faEnvelope, faAngleDown, faSparkles, faCommentSmile, faGlasses, faChartMixedUpCircleDollar, faFilePowerpoint, faFileLines, faFileCode, faHashtag, faBookOpen, faPresentationScreen, faVideo, faBookOpenReader, faChalkboardUser, faLocationDot, faSnowflake, faGithubAlt, faLinkedinIn, faYoutube);
 dom.watch();
