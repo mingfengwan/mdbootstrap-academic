5e5a594c2f46fb70139b0b3ffc5814d31d76535a
mingfengwan <ming@explorerhop.com>
2024-12-14 02:50:09 -0500

    Change two icons from solid duotone to regular duotone

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,9 @@ import { Collapse, Tooltip, Ripple, initMDB } from "mdb-ui-kit";
 initMDB({ Collapse, Tooltip, Ripple });
 
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faPersonDressSimple, faEnvelope, faSparkles, faCommentSmile, faGlasses, faChartMixedUpCircleDollar, faFilePowerpoint, faFileLines, faFileCode, faHashtag, faBookOpen, faPresentationScreen, faVideo, faBookOpenReader, faChalkboardUser, faLocationDot, faSnowflake } from '@fortawesome/pro-duotone-svg-icons';
+import { faPersonDressSimple } from '@fortawesome/duotone-regular-svg-icons/faPersonDressSimple';
+import { faEnvelope } from '@fortawesome/duotone-regular-svg-icons/faEnvelope';
+import { faSparkles, faCommentSmile, faGlasses, faChartMixedUpCircleDollar, faFilePowerpoint, faFileLines, faFileCode, faHashtag, faBookOpen, faPresentationScreen, faVideo, faBookOpenReader, faChalkboardUser, faLocationDot, faSnowflake } from '@fortawesome/pro-duotone-svg-icons';
 import { faBars, faAngleDown } from '@fortawesome/pro-solid-svg-icons';
 import { faImage } from '@fortawesome/duotone-light-svg-icons/faImage';
 import { faGithubAlt, faLinkedinIn, faYoutube } from '@fortawesome/free-brands-svg-icons';
