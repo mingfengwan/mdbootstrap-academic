bb523dc94f0dd6476c1a9355d3e2e9ca3cebe31b
mingfengwan <ming@explorerhop.com>
2024-12-16 17:32:38 -0500

    Modify postcss.config.js to include the css class used in js files, rerun build

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,9 @@
 const purgecss = require('@fullhuman/postcss-purgecss').default;
 
 const purgecssConfig = purgecss({
-    content: ['./dist/*.html', './node_modules/mdb-ui-kit/js/mdb.umd.min.js'],
+    content: ['./dist/*.html', './node_modules/mdb-ui-kit/js/mdb.umd.min.js', './src/js/*.js'],
     css: ['./node_modules/mdb-ui-kit/**/*.css'],
     safelist: {
-        standard: ['heart'], // Keep 'heart' class intact
         deep: [
             /^ripple-surface-*/, // Allow all classes starting with ripple-surface-
             /^ripple-wave$/,     // Allow ripple-wave
